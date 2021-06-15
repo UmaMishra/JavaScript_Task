8f72fffed446cc2267a7ea0d7d002df83d91f7f3
UmaMishra <77145169+UmaMishra@users.noreply.github.com>
2021-06-15 23:13:08 +0530

    Add files via upload

---
+++ conditional_statement.js
@@ -0,0 +1,12 @@
+let greater=5>4;
+console.log(greater);  //true
+
+greater = "apple" > "pineapple"
+console.log(greater);  //false
+
+greater  = "2" > "12";
+console.log(greater);
+
+console.log(null == "\n0\n"); //false
+
+console.log(null === +"\n0\n") //false
